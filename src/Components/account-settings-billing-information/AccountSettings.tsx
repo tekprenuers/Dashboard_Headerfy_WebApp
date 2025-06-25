fee95121100b3f8c593695f4d3b452606cc708c8
betty-19 <betelhemsintayehu10@gmail.com>
2025-06-25 19:35:09 +0100

    working on account setting and billing information

--- src/Components/account-settings-billing-information/AccountSettings.tsx
+++ src/Components/account-settings-billing-information/AccountSettings.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import uploadIcon from '../../assets/image/upload.png';
+import BillingInformation from './BillingInformation';
+
 
 interface AccountSettingsProps {
   onClose: () => void;
+  onBillingClick:()=>void;
 }
 
-const AccountSettings: React.FC<AccountSettingsProps> = ({ onClose }) => {
+const AccountSettings: React.FC<AccountSettingsProps> = ({ onClose,onBillingClick }) => {
   const [logo, setLogo] = useState<string | null>(null);
+  const [showBillingInfo,setShowBillingInfo]= useState(false);
+ 
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -24,8 +29,9 @@ const AccountSettings: React.FC<AccountSettingsProps> = ({ onClose }) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-black/70 flex items-center justify-center backdrop-blur-sm">
-      <div className="w-[811px] bg-white rounded-[15px] p-6 relative">
+    <>
+    <div className="fixed inset-0 bg-black/70 flex items-center justify-center overflow-auto">
+      <div className="w-[811px] max-h-[95vh] overflow-y-auto bg-white rounded-[15px] p-6 relative">
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 text-2xl cursor-pointer "
@@ -70,7 +76,7 @@ const AccountSettings: React.FC<AccountSettingsProps> = ({ onClose }) => {
                 onChange={handleFileChange}
                 className="absolute inset-0 opacity-0 cursor-pointer w-full h-full"
               />
-              {!logo ? (
+              
                 <div className="flex flex-col items-center justify-center pointer-events-none">
                   <img src={uploadIcon} alt="Upload" className="w-[28px] h-[23px]" />
                   <p className="text-[#545454] text-[16px] font-[400] mt-2">
@@ -80,8 +86,11 @@ const AccountSettings: React.FC<AccountSettingsProps> = ({ onClose }) => {
                     (Only *.jpeg, *.webp and *.png images will be accepted)
                   </p>
                 </div>
-              ) : (
-                <div className="relative w-[96px] h-[92px] border border-[#F53434]">
+             
+            </div>
+          </div>
+           {logo &&
+                <div className="relative w-[96px] h-[92px] border bg-[#DCEEFF] flex justify-end ml-[290px]">
                   <img
                     src={logo}
                     alt="Business Logo"
@@ -94,15 +103,15 @@ const AccountSettings: React.FC<AccountSettingsProps> = ({ onClose }) => {
                     ×
                   </button>
                 </div>
-              )}
-            </div>
-          </div>
+              }
 
           {/* Billing & Delete Buttons */}
           <hr className="text-[#DFE0E0] w-full h-[1px] my-4" />
           <div className="flex items-center gap-[230px]">
             <label htmlFor="">Billings</label>
-            <button className="text-[#000000B0] font-[500] text-[14px] w-[149px] h-[43px] border border-[#ECEEF1] rounded-[10px] bg-[#F3F4F6]">
+            <button className="text-[#000000B0] font-[500] text-[14px] w-[149px] h-[43px] border border-[#ECEEF1] rounded-[10px] bg-[#F3F4F6] cursor-pointer"
+            onClick={onBillingClick}
+            >
               Billing Information
             </button>
           </div>
@@ -118,13 +127,19 @@ const AccountSettings: React.FC<AccountSettingsProps> = ({ onClose }) => {
             <button
               type="submit"
               className="w-[97px] h-[27px] bg-[#FF5733] hover:bg-[#E04B2B] text-white text-[10px] font-[600] rounded-[6px] transition-colors"
+              
             >
-              Update
+              Update Profile
             </button>
           </div>
         </div>
       </div>
     </div>
+    
+// Billing Info Modal
+    {showBillingInfo && (<BillingInformation onClose={()=> setShowBillingInfo(false)}/>)}
+    </>
+    
   );
 };
 
