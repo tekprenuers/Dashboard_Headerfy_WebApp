1af074ead48d0784a62176a9ec30d909d92397c6
betty-19 <betelhemsintayehu10@gmail.com>
2025-06-25 19:55:15 +0100

    new branch is created

---
+++ src/Components/MenuDropDownAndEditingMode/MenuDropDown.tsx
@@ -0,0 +1,68 @@
+
+
+import React from 'react'
+
+const MenuDropDown : React.FC = ()=>{
+return(
+    <div>
+        <div>
+            <div>
+                <p>Chims Project</p>
+                <i></i>
+            </div>
+            <hr />
+            <i></i><input type="text" placeholder='Quick Search'/>
+            <div>
+                <p>Home</p>
+                <i></i>
+            </div>
+            <div>
+                <p>Template</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Ai content generator</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Customize</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Export and Download</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Saved Design</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Pricing</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Help & Support</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Trash</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Show Version Histroy</p>
+                <i></i>
+            </div>
+           <div>
+                <p>Suggest improvement</p>
+                <i></i>
+            </div>
+             <div>
+                <p>Log out</p>
+                <i></i>
+            </div>
+
+        </div>
+    </div>
+)
+}
+export default MenuDropDown 
