e1b5a49d9ae61a1d739363c0f594853a948f9316
Betelhem <betelhemsintayehu10@gmail.com>
2025-06-26 15:14:43 +0300

    working on MenuDropDown

--- src/Components/MenuDropDownAndEditingMode/MenuDropDown.tsx
+++ src/Components/MenuDropDownAndEditingMode/MenuDropDown.tsx
@@ -5,60 +5,63 @@ import React from 'react'
 const MenuDropDown : React.FC = ()=>{
 return(
     <div>
-        <div>
-            <div>
-                <p>Chims Project</p>
+        <div className='w-[197px] h-[343px] rounded-[15px] bg-[#0F3F6F] box-shadow-[0px 4px 4px 0px #00000040]
+p-[10px]'>
+            <div className='flex align-center justify-space-between'>
+                <p className='font-[500] text-[14px] text-[#FFFFFF]'>Chims Project</p>
                 <i></i>
             </div>
-            <hr />
-            <i></i><input type="text" placeholder='Quick Search'/>
-            <div>
-                <p>Home</p>
+            <hr className='color-[#FFFFFF] mt-[15px] mb-[10px]'/>
+            <div><i></i><input type="text" placeholder='Quick Search' className='w-[172px] h-[27px]
+rounded-[6px] border border-[#FFFFFF] font[400] text[10px] text-[#FFFFFF]'/></div>
+            
+            <div className="flex align-center justify-space-around pt-[10px]">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Home</p>
                 <i></i>
             </div>
-            <div>
-                <p>Template</p>
+            <div className="flex align-center justify-space-around pt-[5px]">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Template</p>
                 <i></i>
             </div>
-             <div>
-                <p>Ai content generator</p>
+             <div className="flex align-center justify-space-around pt-[10px]">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Ai content generator</p>
                 <i></i>
             </div>
-             <div>
-                <p>Customize</p>
+             <div className="flex align-center justify-space-around pt-[10px]">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Customize</p>
                 <i></i>
             </div>
-             <div>
-                <p>Export and Download</p>
+             <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Export & Download</p>
                 <i></i>
             </div>
-             <div>
-                <p>Saved Design</p>
+             <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Saved Design</p>
                 <i></i>
             </div>
-             <div>
-                <p>Pricing</p>
+             <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Pricing</p>
                 <i></i>
             </div>
-             <div>
-                <p>Help & Support</p>
+             <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Help & Support</p>
                 <i></i>
             </div>
-             <div>
-                <p>Trash</p>
+             <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Trash</p>
                 <i></i>
             </div>
-             <div>
-                <p>Show Version Histroy</p>
+             <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Show Version Histroy</p>
                 <i></i>
             </div>
-           <div>
-                <p>Suggest improvement</p>
+           <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Suggest improvement</p>
                 <i></i>
             </div>
-             <div>
-                <p>Log out</p>
-                <i></i>
+             <div className="flex align-center justify-space-around">
+                <p className="font-[500] text-[10px] text-[#FFFFFF]">Log out</p>
+                <p>8</p>
             </div>
 
         </div>
